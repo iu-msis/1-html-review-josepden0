d67999016e695843ad156ecc02d7fed19c834c87
josepden0 <denis.jose.joseph@gmail.com>
2021-09-21 22:07:02 -0400

    Preliminary Work
    Trying to manipulate js

---
+++ app/public/js/index.js
@@ -0,0 +1,62 @@
+const Counter = 
+{
+    data() 
+        {
+      return { //return json obj
+        "Being": 
+        {
+
+        },  
+        "BigImage":"",
+        "fname":"",
+        "lname":"",
+        "origin":"",
+        "age":"",
+        "email":"",
+        "MedImage": "",
+
+        }
+    },
+    created() //called function to fetch
+    {
+        this.fetchUserData();
+    },
+
+    computed: //Data Manipulation
+    {
+        prettyBirthday()
+        {
+            return dayjs(this.person.dob.date).format('D MMM YYYY');
+        }
+    },
+
+    methods: 
+    {
+        fetchUserData()
+        {
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json()) //Get Promise
+        
+        .then(responseJSON => 
+            {
+            var user = responseJSON.results[0];
+            this.fname = user.name.first;
+            this.lname = user.name.last;
+            this.origin = user.location.country;
+            this.age = user.dob.age;
+            this.email = user.email;
+            this.BigImage = user.picture.large;
+            this.MedImage = user.picture.medium;
+
+            console.log(user);
+            })
+
+        .catch((err) => 
+        {
+            console.error(err);
+        })
+
+        }
+    }
+}
+Vue.createApp(Counter).mount('#testVueApp');
